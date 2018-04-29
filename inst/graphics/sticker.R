28ad322686b9b6873adc1ee252071da1934c21ff
cpanse <cp@fgcz.ethz.ch>
2018-04-29 10:37:46 +0200

    initial

---
+++ inst/graphics/sticker.R
@@ -0,0 +1,15 @@
+library(hexSticker)
+
+sticker("~/octopussy_.png", 
+  package="rawDiag", 
+  filename="rawDiag_sticker.png", 
+  h_fill='white', 
+  h_col = 'cornflowerblue', 
+  h_size = 1.5,
+  p_color = 'cornflowerblue', 
+  spotlight=TRUE, 
+  p_size=18,
+  s_x=1,
+  s_width = 0.45,
+  u_size = 4,
+  url = 'https://github.com/protViz/rawDiag')
