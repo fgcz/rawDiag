e8d47d605ebaa7508b7fcf876e1fa6caf3874de3
Christian Panse <cp@fgcz.ethz.ch>
2018-09-11 15:32:13 +0200

    /optimize   /platform:anycpu

---
+++ exec/build_linux_fgcz_raw.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mcs /out:fgcz_raw.exe \
+  ../inst/docker/fgcz_raw.cs /r:ThermoFisher.CommonCore.Data.dll \
+  /r:ThermoFisher.CommonCore.MassPrecisionEstimator.dll \
+  /r:ThermoFisher.CommonCore.RawFileReader.dll /target:exe \
+  /optimize \
+  /platform:anycpu
