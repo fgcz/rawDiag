bcea1c075c691986209f2643efa47a987be81bda
Christian Panse <cp@fgcz.ethz.ch>
2019-05-29 15:26:48 +0200

    bugfux profile mode

--- exec/build_linux_fgcz_raw.bash
+++ exec/build_linux_fgcz_raw.bash
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# build script Christian Panse <cp@fgcz.ethz.ch> 2018,2019
+
+set -x
+
 mcs /target:library ../src/fgcz_raw.cs  /out:fgcz_raw.dll  \
   /r:ThermoFisher.CommonCore.Data.dll \
   /r:ThermoFisher.CommonCore.MassPrecisionEstimator.dll \
-  /r:ThermoFisher.CommonCore.RawFileReader.dll
+  /r:ThermoFisher.CommonCore.RawFileReader.dll \
+  /optimize \
+  /platform:anycpu
 
 ## apt-get install mono-mcs
 mcs /out:fgcz_raw.exe \
