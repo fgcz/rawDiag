5ec59c19d603333e432a8c9bc90f77b94a5b0cd6
Christian Panse <cp@fgcz.ethz.ch>
2019-06-15 16:31:15 +0200

    #51 initial MsBackendRawDiag(); @jorainer thankyou for mzR template.

--- R/MsBackendRawDiag.R
+++ R/MsBackendRawDiag.R
@@ -1,40 +1,89 @@
-#R 
-# FGCZ, TK,CP 2019-06-14
 
-
-#' Force an peaklistSet Object to Belong to a Class DataFrame
+#' @title rawDiag-based backend
 #'
-#' @param x an \code{\link[protViz]{peaklistSet}}} object.
-#' @param ... 
+#' @description
 #'
-#' @author TK,CP 2019-06-14
-#' @return \code{\link[S4Vectors]{DataFrane} object}
-#' @export as.peaklistSet.DataFrame
-#' @examples 
-#' library(rawDiag)
-#' (rawfile <- file.path(path.package(package = 'rawDiag'), 'extdata', 'sample.raw'))
-#' PLS <-readScans(rawfile)
-#' DF <- as.peaklistSet.DataFrame(PLS)
-#' DF$fromFile = as.integer(1)
+#' The `MsBackendRawDiag` inherits all slots and methods from the base
+#' `MsBackendDataFrame` (in-memory) backend. It overrides the base `mz` and
+#' `intensity` methods as well as `peaks` to read the respective data from
+#' the original raw data files.
+#'
+#' The validator function has to ensure that the files provided in the
+#' `files` slot exist.
+#'
+#' The `backendInitialize` method reads the header data from the raw files and
+#' hence fills the `spectraData` slot. Note that this method could be called
+#' several times, e.g. also to *re-fill* `spectraData` after dropping some of
+#' its columns.
+#'
+#' @author Christian Panse 2019-06-15 
+#' adapted from the MsBackendMzR.R file by Johannes Rainer
 #' 
-#' if(require(Spectra)){
-#'   rawDiagSample <- MsBackendDataFrame()
-#'   BE <- backendInitialize(object=rawDiagSample, files=rawfile, spectraData=DF)
-#' }
-as.peaklistSet.DataFrame <- function(x, ...){
-  if (!require(S4Vectors)){ warning("pkg 'S4Vectors' not installed."); return (NULL) }
-  
-  n <- length(x)
-  df <- DataFrame(
-    msLevel = sapply(x, function(y){as.integer(y$msLevel)}),
-    charge = sapply(x, function(y){as.integer(y$charge)}),
-    rtime = sapply(x, function(y){y$rtinseconds}),
-    scanIndex = sapply(x, function(y){y$scan}),
-    polarity = sapply(x, function(y){as.integer(y$polarity)}),
-    id = rep(NA, n),
-    name = sapply(x, function(y){y$title}))
-  df$mz <- lapply(x, function(y){y$mZ})
-  df$intensity <- lapply(x, function(y){y$intensity})
-  
-  df
-}
+#' @importClassesFrom Spectra MsBackendDataFrame 
+#'
+#' @noRd
+setClass("MsBackendRawDiag",
+         contains = "MsBackendDataFrame",
+         prototype = prototype(version = "0.1", readonly = TRUE))
+
+setValidity("MsBackendRawDiag", function(object) {
+    msg <- Spectra:::.valid_spectra_data_required_columns(object@spectraData,
+                                                c("fromFile", "scanIndex"))
+    msg <- c(msg, Spectra:::.valid_ms_backend_files_exist(object@files))
+    if (length(msg)) msg
+    else TRUE
+})
+
+#' @rdname hidden_aliases
+#'
+#' @importFrom methods callNextMethod
+#'
+#' @importMethodsFrom BiocParallel bpmapply
+#'
+#' @importFrom BiocParallel bpparam
+setMethod("backendInitialize", "MsBackendRawDiag",
+          function(object, files, spectraData, ..., BPPARAM = bpparam()) {
+              if (missing(files) || !length(files))
+                  stop("Parameter 'files' is mandatory for 'MsBackendRawDiag'")
+            
+              files <- normalizePath(files)
+              
+             
+              
+              if (!all(file.exists(files)))
+                  stop("File(s) ", paste(files[!file.exists(files)]),
+                       " not found")
+              msg <- Spectra:::.valid_ms_backend_files(files)
+              if (length(msg))
+                  stop(msg)
+              if (!missing(spectraData)) {
+                  spectraData$mz <- NULL
+                  spectraData$intensity <- NULL
+              } else {
+                
+                  rawfileReaderObj <- lapply(files, function(rawfile){.cnew ("Rawfile", rawfile)})
+                  
+                  spectraData <- do.call(
+                      rbind, mapply(rawfileReaderObj, seq_along(files),
+                                      FUN = function(flObj, index) {
+                                          cbind(rawDiag:::.rawDiag_header(flObj),
+                                                fromFile = index)
+                                      }))
+              }
+              callNextMethod(object = object,
+                             files = files,
+                             spectraData = spectraData,
+                             ...)
+          })
+
+setMethod("show", "MsBackendRawDiag", function(object) {
+    callNextMethod()
+    fls <- basename(object@files)
+    if (length(fls)) {
+        to <- min(3, length(fls))
+        cat("\nfile(s):\n", paste(basename(fls[1:to]), collapse = "\n"),
+            "\n", sep = "")
+        if (length(fls) > 3)
+            cat(" ...", length(fls) - 3, "more files\n")
+    }
+})
