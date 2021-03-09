5a25e2711dcefb87d4e8b6a5a05661ae2f79930b
Christian Panse <cp@fgcz.ethz.ch>
2021-03-09 08:16:45 +0100

    added copyright

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,8 @@
 .onAttach <- function(lib, pkg){
 	if(interactive()){
 		version <- packageVersion('rawDiag')
-		packageStartupMessage("Package 'rawDiag' version ", version)
+		thermocopyright <- "RawFileReader reading tool. Copyright \u00A9 2016 by Thermo Fisher Scientific, Inc. All rights reserved."
+		packageStartupMessage("Package 'rawDiag' version ", version , " using\n", thermocopyright)
 	
 	  invisible()
 	}
