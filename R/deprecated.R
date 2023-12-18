3ec86d2243d3e2d68c472157d1d0be95446fbd8a
Christian Panse <cp@fgcz.ethz.ch>
2023-12-18 10:01:58 +0100

    remove whitespace at the end of the lines
    %s/\ $//

--- R/deprecated.R
+++ R/deprecated.R
@@ -3,58 +3,58 @@
 
 #' @export
 PlotLockMassCorrection <- function(x, method = 'trellis'){
-	.Deprecated("plotLockMassCorrection")
-	plotLockMassCorrection(x, method)
+    .Deprecated("plotLockMassCorrection")
+    plotLockMassCorrection(x, method)
 }
 
 #' @export
 PlotPrecursorHeatmap <- function(x, method = 'overlay', bins = 80){
-	.Deprecated("plotPrecursorHeatmap")
-	plotPrecursorHeatmap(x, method, bins)
-}	
+    .Deprecated("plotPrecursorHeatmap")
+    plotPrecursorHeatmap(x, method, bins)
+} 
 
 #' @export
 PlotTicBasepeak <- function(x, method = 'trellis'){
-	.Deprecated("plotTicBasepeak")
-	plotTicBasepeak(x, method)
+    .Deprecated("plotTicBasepeak")
+    plotTicBasepeak(x, method)
 }
 
 
 #' @inherit plotCycleTime params return references author title
 PlotCycleTime <- function(x, method = 'trellis'){
-	.Deprecated("plotCycleTime")
-	plotCycleTime(x, method)
+    .Deprecated("plotCycleTime")
+    plotCycleTime(x, method)
 }
 
 #' @export
 PlotInjectionTime <- function(x, method = 'trellis'){
-	.Deprecated("plotInjectionTime")
-	plotInjectionTime(x, method)
+    .Deprecated("plotInjectionTime")
+    plotInjectionTime(x, method)
 }
 
 
 
 #' @export
-PlotMzDistribution <- function(x, method = 'trellis'){ 
+PlotMzDistribution <- function(x, method = 'trellis'){
     .Deprecated("plotMzDistribution")
     plotMzDistribution(x, method)
 }
 
 #' @export
-PlotMassDistribution <- function(x, method = 'trellis'){ 
-	.Deprecated("plotMassDistribution")
-	plotMassDistribution(x, method)
+PlotMassDistribution <- function(x, method = 'trellis'){
+    .Deprecated("plotMassDistribution")
+    plotMassDistribution(x, method)
 }
 
 
 #' @export
-PlotChargeState <- function(x, method = 'trellis'){ 
+PlotChargeState <- function(x, method = 'trellis'){
   .Deprecated("plotChargeState")
   plotChargeState(x, method)
 }
 
 #' @export
-PlotScanTime <- function(x, method = 'trellis'){ 
+PlotScanTime <- function(x, method = 'trellis'){
   .Deprecated("plotScanTime")
   plotScanTime(x, method)
 }
